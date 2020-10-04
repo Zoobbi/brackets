eea83c702beab3ff27bd67e4f73916df4f8ea389
Oleg <karibyon@gmail.com>
2020-10-04 13:49:55 +0300

    feat: complite brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let symbols = str.split(''),
+        [newBrackets,result] = [[],[]];
+
+    for(let i = 0; i < bracketsConfig.length; i++) {
+        newBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    for(let symbol of symbols) {
+        if(symbol === result[result.length - 1]) {
+            result.pop();
+        } else {
+            if(newBrackets[symbol] === undefined) {
+                return false;
+            } else {
+                result.push(newBrackets[symbol]);
+            }
+        }
+    }
+    return result.length == 0;
 }
